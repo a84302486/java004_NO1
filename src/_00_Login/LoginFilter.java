5e99dd84b7b067db75e97cb38d0731045a7d23bf
PC-35\PC-26 <leo12345678996@gmail.com>
2017-03-01 13:47:49 +0800

    登入檢查

---
+++ src/_00_Login/LoginFilter.java
@@ -0,0 +1,102 @@
+package _00_Login;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter(urlPatterns = { "/*" }, initParams = {
+		@WebInitParam(name = "url_1", value = "/_14_Member/*"),
+		@WebInitParam(name = "url_1", value = "/_01_ProductTest/*")
+		})
+public class LoginFilter implements Filter {
+	Collection<String> url = new ArrayList<String>();
+
+	public void destroy() {
+
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		if (request instanceof HttpServletRequest
+				&& response instanceof HttpServletResponse) {
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpServletResponse resp = (HttpServletResponse) response;
+			String servletPath = req.getServletPath();
+			System.out.println("servletPath " + servletPath);
+			if (mustLogin(servletPath)) {
+				if (checkLogin(req)) {
+					System.out.println("需要Login, 已經Login");
+					chain.doFilter(request, response);
+				} else {
+					HttpSession session = req.getSession();
+					session.setAttribute("target", req.getServletPath());
+					System.out.println("需要Login, 尚未Login , ServletPath="
+							+ req.getServletPath());
+					RequestDispatcher rd = request
+							.getRequestDispatcher("/_00_Login/login.jsp");
+					rd.forward(req, resp);
+				}
+			} else {
+				System.out.println("不需要Login ");
+				chain.doFilter(request, response);
+			}
+		} else {
+			throw new ServletException("Request/Response Type Error");
+		}
+	}
+
+	private boolean mustLogin(String servletPath) {
+		boolean login = false;
+		for (String sURL : url) {
+			if (sURL.endsWith("*")) {
+				sURL = sURL.substring(0, sURL.length() - 1);
+				if (servletPath.startsWith(sURL)) {
+					login = true;
+					break;
+				}
+			} else {
+				if (servletPath.equals(sURL)) {
+					login = true;
+					break;
+				}
+			}
+		}
+		return login;
+	}
+
+	private boolean checkLogin(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		UserBean loginToken = (UserBean) session.getAttribute("LoginOK");
+		if (loginToken == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		Enumeration<String> e = fConfig.getInitParameterNames();
+		while (e.hasMoreElements()) {
+			String name = e.nextElement();
+			url.add(fConfig.getInitParameter(name));
+		}
+	}
+}
