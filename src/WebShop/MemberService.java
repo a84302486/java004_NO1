cc5487f9c4631397a0ca2072856cc4b6899fbf75
PC-35\PC-26 <leo12345678996@gmail.com>
2017-04-13 18:15:09 +0800

    加密解密修改

--- src/WebShop/MemberService.java
+++ src/WebShop/MemberService.java
@@ -2,11 +2,12 @@ package WebShop;
 
 import java.util.Collection;
 
+import _00_Util.AES;
 import _14_Member.MemberBean;
 import _14_Member.MemberDAO;
 
 public class MemberService {
-	public MemberBean checkIDPassword(String Username, String Password) {
+	public MemberBean checkIDPassword(String Username, String Password) throws Exception {
 		// 將 MemberDAO new為物件，放入變數dao內
 		MemberDAO dao = new MemberDAO();
 		// 透過變數dao，呼叫它的select()方法，要傳入參數 id。將傳回值放入變數
@@ -19,9 +20,14 @@ public class MemberService {
 			return null;
 		}
         // 如果mb不等於 null 而且參數 password等於mb內的password) {
-        if ( mb != null && Password.equals(mb.getM_Password())) {
-        	// 傳回 mb物件，同時結束本方法
-        	 return mb;
+        if ( mb != null ) {
+        	String passwordDecrypt = AES.decrypt(mb.getM_Password());
+        	
+        	if(Password.equals(passwordDecrypt)) {
+      	
+        		// 傳回 mb物件，同時結束本方法       	 
+        		return mb;
+        	} 
         }
         // 傳回null物件
 		return null;
--- src/_00_Util/SystemConstant.java
+++ src/_00_Util/SystemConstant.java
@@ -6,12 +6,8 @@ public class SystemConstant {
     public static final String HOST = "127.0.0.1";
     public static final String DB = "Java004NO1";
     public static final String USER = "root";
-<<<<<<< HEAD
-    public static final String PASSWORD = "215060";
-=======
     public static final String PASSWORD = "password";
 
->>>>>>> 9c69e78be1c01e6f5cd0337079a89c85a6578c72
     public static final String URL = "jdbc:mysql://" + HOST + ":3306/" + SystemConstant.DB
 			+ "?user=" + SystemConstant.USER + "&password="
 			+ SystemConstant.PASSWORD + "&useSSL=true&useUnicode=yes&characterEncoding=UTF-8";
